54c24cdf962c011d7300c47a74b78da09989d102
wt1205 <djung.051205@gmail.com>
2025-06-01 10:02:42 +0900

    UI

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public GameObject Pause;
+    public GameObject PauseMenu;
+
+    public void ClickOpenMenu(){
+        Pause.SetActive(false);
+        PauseMenu.SetActive(true);
+    }
+
+    public void ClickResume(){
+        Pause.SetActive(true);
+        PauseMenu.SetActive(false);
+    }
+
+    public void ClickExit(){
+        
+    }
+
+    public void ClickRestart(){
+
+    }
+
+    public void ClickStart(){
+        SceneManager.LoadScene("Stage");
+    }
+
+    public void ClickQuit(){
+        Application.Quit();
+    }
+}

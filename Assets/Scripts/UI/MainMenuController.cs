237c311e6e3eba26c501c682f5b2464db3040b97
Zzalbs <kcjlazer@naver.com>
2025-06-07 14:27:06 +0900

    메인메뉴 정보 UI 완성

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -3,11 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] GameObject gameInfoUI;
+
+    private void Awake()
+    {
+        ClearScreen();
+    }
+
     public void ClickStart(){
         SceneManager.LoadScene("Stage");
     }
 
+    public void ClickInfo()
+    {
+        gameInfoUI.SetActive(true);
+    }
+
     public void ClickQuit(){
+        ClearScreen();
         Application.Quit();
+    }
+
+    public void ClearScreen()
+    {
+        gameInfoUI.SetActive(false);
     }
 }
